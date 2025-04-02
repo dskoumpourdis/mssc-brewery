47e7e9f97bfc273cd4412e0c8d4be4d591eb871b
Dimitris <nomix_xx@hotmai.com>
2025-04-02 17:42:31 +0100

    Add dataloader. Closes #8

--- src/main/java/com/dskoumpo/mssc_brewery/repository/BeerRepository.java
+++ src/main/java/com/dskoumpo/mssc_brewery/repository/BeerRepository.java
@@ -1,10 +1,10 @@
 package com.dskoumpo.mssc_brewery.repository;
 
 import com.dskoumpo.mssc_brewery.domain.Beer;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 
-public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+public interface BeerRepository extends JpaRepository<Beer, UUID> {
 }
